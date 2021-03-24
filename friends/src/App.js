81e887c545549847eb76100ce8eaa6b9bd97bb30
Taylor Friesen <taylorfriesen@protonmail.com>
2021-03-23 20:53:25 -0700

    friends can be added

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Login from './components/Login';
 import FriendsList from './components/FriendsList';
+import NewFriend from './components/NewFriend';
 import PrivateRoute from './components/PrivateRoute';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route path="/login" component={Login} />
           <PrivateRoute path="/list" component={FriendsList} />
+          <PrivateRoute path="/add" component={NewFriend} />
         </Switch>
     </Router>
   );
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,19 +1,24 @@
 import {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const FriendsList = (props) => {
   const [friends, setFriends] = useState([]);
+  const [message, setMessage] = useState('');
   useEffect(() => {
+    setMessage('Loading friends list...')
     axiosWithAuth().get('/friends').then(res => {
       setFriends([...res.data]);
+      setMessage('');
     }).catch(err => {
-      console.log(err);
+      setMessage('Failed to load friends :(')
     });
   }, [])
   return (
     <div>
-      {/* <pre>{JSON.stringify(friends, null, '\t')}</pre> */}
+      <div>{message}</div>
       {friends.map(friend => <div>{friend.name}, age {friend.age}, can be emailed at {friend.email}</div>)}
+      <div><Link to='/add'>Add a new friend</Link></div>
     </div>)
 }
 
